72c90aa053f5db0c9eeb77a1d8e306aaf5aaa02b
Tacohej <mctinn@gmail.com>
2018-08-12 08:37:05 +0200

    art n stuff

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParallaxScript : MonoBehaviour
+{
+	public float parallaxWeight;
+
+	void Update ()
+	{
+		var parallaxPosition = Camera.main.transform.position * parallaxWeight;
+		var position = this.transform.position;
+		position.x = parallaxPosition.x;
+		// position.y = parallaxPosition.y;
+		this.transform.position = position;
+	}
+}
